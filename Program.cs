ff0b8592150a63661eaf622afee95dbb269f6985
chinhyanglong <chinhyanglong@gmail.com>
2020-08-13 17:15:20 +0700

    new code

--- Program.cs
+++ Program.cs
@@ -22,5 +22,25 @@ namespace huyblog
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-    }
+        ///
+        ///  public static void Main(string[] args)
+        //{
+        //    CreateWebHostBuilder(args)
+        //        .UseKestrel()
+        //        .UseUrls("http://0.0.0.0:" + Environment.GetEnvironmentVariable("PORT"))
+        //        .Build()
+        //        .Run();
+        //}
+
+
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .ConfigureLogging((logging) =>
+        //        {
+        //            logging.AddDebug();
+        //            logging.AddConsole();
+        //        })
+        //        .UseStartup<Startup>();
+    ///
+}
 }
